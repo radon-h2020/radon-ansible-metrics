22d2df8d6e26b164cbfcc9bc97c22b3e70d5ab6b
Stefano Dalla Palma <stefano.dallapalma0@gmail.com>
2023-08-27 23:17:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = '0.4.1'
+VERSION = '0.4.2'
 
 setup(name='ansiblemetrics',
       version=VERSION,
